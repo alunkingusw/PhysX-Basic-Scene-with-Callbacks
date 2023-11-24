fc2fb94d23cc243ef4473cf072292cd0eae3c0cb
Alun King <alun.king1@southwales.ac.uk>
2023-11-24 13:31:13 +0000

    Trying to add controller filter to sort collisions itself.

--- PhysX Basic Scene/CharacterController.cpp
+++ PhysX Basic Scene/CharacterController.cpp
@@ -12,6 +12,7 @@ CharacterController::~CharacterController() {
 
 void CharacterController::createCharacter(const physx::PxExtendedVec3& position, physx::PxMaterial* characterMaterial) {
     // Create capsule controller
+    //characterHitReport = new CharacterControllerHitReport();
     physx::PxCapsuleControllerDesc desc;
     desc.setToDefault();
     desc.radius = 0.5f;
@@ -19,6 +20,8 @@ void CharacterController::createCharacter(const physx::PxExtendedVec3& position,
     desc.upDirection = physx::PxVec3(0, 1, 0);
     desc.position = position;
     desc.material = characterMaterial;
+    //desc.reportCallback = characterHitReport;
+    // Add the actor to the scene
     capsuleController = static_cast<physx::PxCapsuleController*>(controllerManager->createController(desc));
 
     // Add the actor to the scene
@@ -47,7 +50,15 @@ void CharacterController::moveForward(float distance) {
         physx::PxVec3 movement = forwardDir * distance;
 
         // Move the controller
-        physx::PxControllerFilters filters;
+        
+        
+
+        // Add objects to the filter
+        physx::PxFilterData filterData;
+        filterData.word0 = FilterGroup::eBOX; // Use your specific filter group
+        filterData.word1 = FilterGroup::eFLOOR; // Use your specific filter group
+
+        physx::PxControllerFilters filters(filterData);
         capsuleController->move(movement, 0.0f, 0.001f, filters);
 
     }
@@ -77,6 +88,14 @@ void CharacterController::moveLeft(float distance) {
 
         // Move the controller
         physx::PxControllerFilters filters;
+        
+
+        // Add objects to the filter
+        physx::PxFilterData filterData;
+        filterData.word0 = FilterGroup::eBOX; // Use your specific filter group
+        filterData.word1 = FilterGroup::eFLOOR; // Use your specific filter group
+
+        filters.setFilterData(filterData);
         capsuleController->move(movement, 0.0f, 0.001f, filters);
 
     }
